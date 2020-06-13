c1f4e1365c7c2b9fa746148745f16b8d04cb22de
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-06-13 16:52:34 +0000

    #27 [create] 管理者側の商品ページ

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -1,10 +1,13 @@
 class Admins::ProductsController < ApplicationController
 
 	def index
+		@products = Product.order(created_at: :asc).page(params[:page])
 	end
 
 	def new
-	end
+        @product = Product.new
+        @active_category = Category.where(is_active: true)
+    end
 
 	def show
 	end
