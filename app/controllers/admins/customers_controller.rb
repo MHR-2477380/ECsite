0f6aa6b08650efa9e183dbd93998802718122c2d
MHR-2477380 <ru.ru.ru.247738@gmail.com>
2020-06-26 17:42:34 +0000

    #38 [create] 管理者側のカスタマーページ

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -9,9 +9,23 @@ class Admins::CustomersController < ApplicationController
 	end
 
 	def edit
+		@customer =  Customer.find(params[:id])
 	end
 
 	def update
-	end
+        @customer = Customer.find(params[:id])
+        if @customer.update(customer_params)
+            flash[:notice] = "顧客情報を編集しました。"
+            redirect_to(admins_customer_path(@customer))
+        else
+            @customer =  Customer.find(params[:id])
+            render(:edit)
+        end
+    end
 
 end
+
+private
+    def customer_params
+        params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :phone_number, :email, :is_active)
+    end
